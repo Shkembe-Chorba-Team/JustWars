2fc1ccf619030c49cdde913ee8043c429505b323
Emil Nik <emil.nik1002@gmail.com>
2016-01-25 19:17:03 +0200

    Reformatting

--- Source/JustWars.Web/Models/Battle.cs
+++ Source/JustWars.Web/Models/Battle.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JustWars.Web.Models
+﻿namespace JustWars.Web.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Battle
     {
         public int Id { get; set; }
