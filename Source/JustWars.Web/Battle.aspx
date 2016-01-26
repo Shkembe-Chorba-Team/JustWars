22de8bdfe87cba2997a3be9dbd9bd6bd330c9499
Emil Nik <emil.nik1002@gmail.com>
2016-01-26 03:44:14 +0200

    Added battle game logic

---
+++ Source/JustWars.Web/Battle.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Battle.aspx.cs" Inherits="JustWars.Web.Battle" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Battle</h1>
+    <asp:Label ID="winnerLabel" runat="server"></asp:Label>
+</asp:Content>
---
+++ Source/JustWars.Web/Controllers/GoldController.cs
@@ -0,0 +1,22 @@
+﻿namespace JustWars.Web.Controllers
+{
+    using Common.Constants;
+    using JustWars.Common;
+    using Models;
+
+    public class GoldController
+    {
+        public static void AddGold(User user, int wins)
+        {
+            var gold = CalculateGoldToAdd(wins);
+            // TODO: add gold to user
+        }
+
+        private static uint CalculateGoldToAdd(int wins)
+        {
+            int modifier = RandomGenerator.GenerateRandomNumber(GoldConstants.MinGold, GoldConstants.MaxGold);
+            int gold = modifier / wins;
+            return (uint)gold;
+        }
+    }
+}
