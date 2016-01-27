a3494870b102e548f3681a10699ad5102291bd06
Emil Nik <emil.nik1002@gmail.com>
2016-01-27 14:08:11 +0200

    Added shop (not tested)

---
+++ Source/JustWars.Web/Shop.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Shop.aspx.cs" Inherits="JustWars.Web.Shop" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Shop</h1>
+    <h3>Your gold is  <strong id="gold" runat="server"></strong></h3>
+    <h3 id="error" runat="server" visible="false">You don't have enough gold!</h3>
+    <asp:GridView runat="server" ID="ShopItemsGrid"
+        AllowPaging="true"
+        PageSize="10"
+        ItemType="JustWars.Web.Models.Item" DataKeyNames="Id"
+        AllowSorting="true"
+        AutoGenerateColumns="false"
+        OnPageIndexChanging="ShopItemsGrid_PageIndexChanging"
+        OnSorting="ShopItemsGrid_Sorting"
+        CssClass="col-md-9">
+        <Columns>
+            <asp:DynamicField DataField="Name" />
+            <asp:DynamicField DataField="Strength" />
+            <asp:DynamicField DataField="Defence" />
+            <asp:DynamicField DataField="Stamina" />
+            <asp:DynamicField DataField="Agility" />
+            <asp:DynamicField DataField="Charisma" />
+            <asp:DynamicField DataField="Gold" />
+            <asp:TemplateField ShowHeader="False">
+                <ItemTemplate>
+                    <asp:Button ID="ShowUserButton" runat="server"
+                        Text="Buy" OnCommand="BuyItem" CommandArgument="<%# Item.Id %>" CssClass="btn btn-success" />
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+</asp:Content>
---
+++ Source/JustWars.Web/Shop.aspx.cs
@@ -0,0 +1,89 @@
+﻿namespace JustWars.Web
+{
+    using System;
+    using System.Linq;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
+    using Models;
+
+    public partial class Shop : Page
+    {
+        private JustWarsDbContext dbcontext;
+        private User user;
+        private Item[] items;
+        private Item selectedItem;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.dbcontext = new JustWarsDbContext();
+            this.user = this.dbcontext.Users.FirstOrDefault(u => u.UserName == this.User.Identity.Name);
+            this.items = this.dbcontext.Items.ToArray();
+            this.gold.InnerText = user.Gold.ToString();
+
+            this.ShopItemsGrid.DataSource = this.items;
+            this.ShopItemsGrid.DataBind();
+
+            Session["ItemsGrid"] = items;
+
+            ShopItemsGrid.DataSource = Session["ItemsGrid"];
+            ShopItemsGrid.DataBind();
+            return;
+        }
+
+        protected void ShopItemsGrid_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            this.ShopItemsGrid.PageIndex = e.NewPageIndex;
+            this.ShopItemsGrid.DataSource = this.items;
+            this.ShopItemsGrid.DataBind();
+        }
+
+        protected void ShopItemsGrid_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            //this.users.Sort = e.SortExpression + " " + GetSortDirection(e.SortExpression);
+            //dt.DefaultView.Sort = e.SortExpression + " " + GetSortDirection(e.SortExpression);
+            //usersGrid.DataSource = Session["UsersGrid"];
+            //usersGrid.DataBind();
+        }
+
+        private string GetSortDirection(string column)
+        {
+            string sortDirection = "ASC";
+            string sortExpression = ViewState["SortExpression"] as string;
+
+            if (sortExpression != null)
+            {
+                if (sortExpression == column)
+                {
+                    string lastDirection = ViewState["SortDirection"] as string;
+                    if ((lastDirection != null) && (lastDirection == "ASC"))
+                    {
+                        sortDirection = "DESC";
+                    }
+                }
+            }
+
+            ViewState["SortDirection"] = sortDirection;
+            ViewState["SortExpression"] = column;
+
+            return sortDirection;
+        }
+
+        protected void BuyItem(object sender, CommandEventArgs e)
+        {
+            var id = e.CommandArgument.ToString();
+            this.selectedItem = this.dbcontext.Items.Find(id);
+
+            if (this.user.Gold >= this.selectedItem.Gold)
+            {
+                this.user.Items.Add(this.selectedItem);
+                this.user.Gold -= (int)this.selectedItem.Gold;
+                this.dbcontext.SaveChanges();
+            }
+            else
+            {
+                this.error.Visible = true;
+            }
+        }
+    }
+}
